50a521fd52450530e7bed5da002ab4e021222d25
Wassim AZIRAR <wa@immosynergies.ch>
2022-09-30 16:17:33 +0200

    first complete version

--- Upcome.Api/Middlewares/ErrorLoggingMiddleware.cs
+++ Upcome.Api/Middlewares/ErrorLoggingMiddleware.cs
@@ -1,5 +1,5 @@
 using System.Net;
-using System.Security.Authentication;
+using System.Text.Json;
 using Upcome.Api.Common;
 
 namespace Upcome.Api.Middlewares;
@@ -29,37 +29,29 @@ public class ErrorLoggingMiddleware
         {
             await _next(context);
         }
-        catch (FileNotFoundException exception)
+        catch (Exception error)
         {
-            await HandleExceptionAsync(context ?? new DefaultHttpContext(), exception);
-        }
-        catch (WebException exception)
-        {
-            await HandleExceptionAsync(context ?? new DefaultHttpContext(), exception);
-        }
-        catch (AuthenticationException exception)
-        {
-            await HandleExceptionAsync(context ?? new DefaultHttpContext(), exception);
-        }
-        catch (Exception exception)
-        {
-            await HandleExceptionAsync(context ?? new DefaultHttpContext(), exception);
-        }
-    }
+            var response = context.Response;
+            response.ContentType = "application/json";
 
-    /// <summary>
-    /// handle exception
-    /// </summary>
-    /// <param name="context">HTTP context</param>
-    /// <param name="exception">Exception</param>
-    private Task HandleExceptionAsync(HttpContext context, Exception exception)
-    {
-        context.Response.ContentType = "application/json";
-        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-        return context.Response.WriteAsync(new ErrorDetails
-        {
-            StatusCode = context.Response.StatusCode,
-            Message = "Internal Server Error"
-        }.ToString());
+            switch(error)
+            {
+                case AppException exception:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case KeyNotFoundException exception:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case FileNotFoundException exception:
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(new { message = error.Message });
+            await response.WriteAsync(result);
+        }
     }
 }
--- Upcome.Api/Program.cs
+++ Upcome.Api/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO.Compression;
+using System.Net;
 using System.Reflection;
 using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
 using Upcome.Api.Common;
 using Upcome.Api.Middlewares;
@@ -17,13 +19,12 @@ var builder = WebApplication.CreateBuilder(args);
 // --------- Add services to the container -----------
 // ---------------------------------------------------
 
-// needed to load configuration from appsettings.json
+// load configuration from appsettings.json
 builder.Services.AddOptions();
 
 // store rate limit counters and ip rules
 builder.Services.AddMemoryCache();
 
-// load general configuration from appsettings.json
 // inject counter, rules stores, resolvers and counter key builders
 builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
 builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
@@ -32,6 +33,73 @@ builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>()
 builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
 builder.Services.AddInMemoryRateLimiting();
 
+// https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-6.0#configure-nginx
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
+
+// Configure HTTP Strict Transport Security Protocol (HSTS)
+// Enforce HTTPS in production
+// => Require HTTPS for all requests
+// => Redirect all HTTP requests to HTTPS
+if (builder.Environment.IsDevelopment() == false)
+{
+    builder.Services.AddHsts(options =>
+    {
+        options.Preload = true;
+        options.IncludeSubDomains = true;
+        options.MaxAge = TimeSpan.FromDays(14);
+    });
+
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 443;
+    });
+}
+
+// Configure CORS, see more https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CORS_POLICY, policy => policy.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
+});
+
+// compression algorithms
+builder.Services.AddResponseCompression(options =>
+{
+    // disabled by default because of the security risk
+    // https://learn.microsoft.com/en-us/aspnet/core/performance/response-compression?view=aspnetcore-6.0#risk
+    options.EnableForHttps = true;
+    
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes = new[] { "application/pdf", "application/json", "application/xml", "text/*" };
+
+});
+builder.Services.Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
+builder.Services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
+
+// reduces the number of requests a client or proxy makes to a web server
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 10 * 1024 * 1024; // The default value is 64 * 1024 * 1024 (64 MB)
+    options.SizeLimit = 18 * 1024 * 1024;       // The default value is 100 * 1024 * 1024 (100 MB)
+    options.UseCaseSensitivePaths = true;       // The default value is false
+});
+
+// Add SECO HttpClient
+builder.Services.AddHttpClient("seco", client =>
+{
+    client.BaseAddress = new Uri("https://entsendung.admin.ch/Lohnrechner/api");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+// adding health check services
+builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecksUI()
+                .AddInMemoryStorage();
+
 // configure MVC services for controllers
 builder.Services.AddControllers();
 
@@ -39,28 +107,7 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
-// adding health check services to container
-builder.Services.AddHealthChecks();
-builder.Services.AddHealthChecksUI()
-                .AddInMemoryStorage();
-
-// compression algorithms
-builder.Services.AddResponseCompression(options =>
-{
-    options.EnableForHttps = true;
-    options.Providers.Add<BrotliCompressionProvider>();
-    options.Providers.Add<GzipCompressionProvider>();
-});
-builder.Services.Configure<BrotliCompressionProviderOptions>(options => options.Level = CompressionLevel.Fastest);
-builder.Services.Configure<GzipCompressionProviderOptions>(options => options.Level = CompressionLevel.Optimal);
-
 builder.Services.AddControllers();
-
-// https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-nginx?view=aspnetcore-6.0#configure-nginx
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -81,36 +128,6 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlPath);
 });
 
-// Configure HTTP Strict Transport Security Protocol (HSTS)
-// Enforce HTTPS in production
-// => Require HTTPS for all requests
-// => Redirect all HTTP requests to HTTPS
-if (builder.Environment.IsDevelopment() == false)
-{
-    builder.Services.AddHsts(options =>
-    {
-        options.Preload = true;
-        options.IncludeSubDomains = true;
-        options.MaxAge = TimeSpan.FromDays(14);
-    });
-
-    builder.Services.AddHttpsRedirection(options => options.HttpsPort = 443);
-}
-
-// Configure CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: CORS_POLICY, policy => policy.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
-});
-
-// reduces the number of requests a client or proxy makes to a web server
-builder.Services.AddResponseCaching(options =>
-{
-    options.MaximumBodySize = 10 * 1024 * 1024; // The default value is 64 * 1024 * 1024 (64 MB)
-    options.SizeLimit = 18 * 1024 * 1024;       // The default value is 100 * 1024 * 1024 (100 MB)
-    options.UseCaseSensitivePaths = true;       // The default value is false
-});
-
 #endregion
 
 #region app.Use
@@ -120,8 +137,8 @@ builder.Services.AddResponseCaching(options =>
 // -------------------------------------------------------
 var app = builder.Build();
 
+// should be registered before any other components
 app.UseIpRateLimiting();
-app.UseClientRateLimiting();
 
 app.UseForwardedHeaders();
 
@@ -138,21 +155,39 @@ else
     // The default HSTS value is 30 days. see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     
+    // HTTPS redirection
     app.UseHttpsRedirection();
 }
 
-app.UseStaticFiles();
+// profiles resumes folders
+var resumesFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "resumes");
 
+// create resumes folder if it does not exist
+Directory.CreateDirectory(resumesFolder);
+
+// use static files for storing them physically on the hard drive
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(resumesFolder),
+    RequestPath = "/resumes"
+});
+
+// it's very important that UsePathBase() is placed before UseRouting()
 app.UseRouting();
 
 // allows restricted resources on the api to be requested
 app.UseCors(CORS_POLICY);
 
+// After UseRouting(), so that route information is available for authentication decisions.
+// Before UseEndpoints(), so that users are authenticated before accessing the endpoints.
 app.UseAuthentication();
 
+// In between the Routing and UseEndpoints middleware
+// Authorisation will check whether the browser is authorised to access the information it is requesting.
 app.UseAuthorization();
 
-// use only if unable to access server-based compression (https://docs.nginx.com/nginx/admin-guide/web-server/compression/)
+// use only if unable to access server-based compression https://docs.nginx.com/nginx/admin-guide/web-server/compression
 app.UseResponseCompression();
 
 // determine when responses are cacheable, stores responses, and serves responses from cache.
@@ -162,13 +197,9 @@ app.UseResponseCaching();
 app.UseMiddleware<ErrorLoggingMiddleware>();
 
 app.UseHealthChecks("/health");
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapDefaultControllerRoute();
+app.UseHealthChecksUI(config => config.UIPath = "/health-ui");
 
-    endpoints.MapHealthChecks("/health");
-    endpoints.MapHealthChecksUI();
-});
+app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
 
 // Enable middleware to serve generated Swagger as a JSON endpoint.
 app.UseSwagger();
