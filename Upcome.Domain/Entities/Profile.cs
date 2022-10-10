ac8f4eaa8166c1670521694ee5279baed5d7838b
Wassim AZIRAR <wa@immosynergies.ch>
2022-10-10 14:47:50 +0200

    work in progress

---
+++ Upcome.Domain/Entities/Profile.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Upcome.Domain.Entities;
+
+[Table("Profile")]
+public class Profile
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public DateTime? CreationDate { get; set; } = DateTime.Now;
+
+    public DateTime? UpdateDate { get; set; } = DateTime.Now;
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Degree { get; set; }
+
+    public string School { get; set; }
+
+    public string City { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Linkedin { get; set; }
+
+    public int ExperienceDuration { get; set; }
+
+    public string Languages { get; set; }
+
+    public double Salary { get; set; }
+
+    public string ResumeUrl { get; set; }
+
+    public string ParsedResume { get; set; }
+}
---
+++ Upcome.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Upcome.Domain.Interfaces;
+
+public interface IRepositoryBase<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+
+    Task<IEnumerable<T?>> GetAllAsync();
+
+    Task<T> AddAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task RemoveAsync(int id);
+}
